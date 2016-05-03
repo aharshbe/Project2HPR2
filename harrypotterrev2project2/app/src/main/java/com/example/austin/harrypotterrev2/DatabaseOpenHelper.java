bd009782bf4ad27d7f399485d75f8e469db81f12
aharshbe <a.catawampus@gmail.com>
2016-05-03 13:48:00 -0700

    Adding the DatabaseOpenHelper class to the java

---
+++ harrypotterrev2project2/app/src/main/java/com/example/austin/harrypotterrev2/DatabaseOpenHelper.java
@@ -0,0 +1,18 @@
+package com.example.austin.harrypotterrev2;
+
+/**
+ * Created by austin on 5/3/16.
+ */
+
+        import android.content.Context;
+
+        import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
+
+public class DatabaseOpenHelper extends SQLiteAssetHelper {
+    private static final String DATABASE_NAME = "movies.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public DatabaseOpenHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+}
