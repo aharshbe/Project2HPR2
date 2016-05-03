2e921833cdd58f6bbeb0c855aeddae23a1eae938
aharshbe <a.catawampus@gmail.com>
2016-05-03 13:53:24 -0700

    Adding information for onCreate to add the database info to the listView and display it using the arrayAdatper

--- harrypotterrev2project2/app/src/main/java/com/example/austin/harrypotterrev2/MainActivity.java
+++ harrypotterrev2project2/app/src/main/java/com/example/austin/harrypotterrev2/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.austin.harrypotterrev2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+        DatabaseAccess databaseAccess = DatabaseAccess.getInstance(this);
+        databaseAccess.open();
+        List<String> quotes = databaseAccess.getMovies();
+        databaseAccess.close();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, quotes);
+        listView.setAdapter(adapter);
+    }
+
+
+
+
     }
 }
