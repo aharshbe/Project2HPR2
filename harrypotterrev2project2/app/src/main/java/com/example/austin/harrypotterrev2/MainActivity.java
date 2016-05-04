f5d657624a07b9f7cf75412fa20606c5fa320d8b
aharshbe <a.catawampus@gmail.com>
2016-05-04 10:40:33 -0700

    Adding onCreate options menu to the main activity

--- harrypotterrev2project2/app/src/main/java/com/example/austin/harrypotterrev2/MainActivity.java
+++ harrypotterrev2project2/app/src/main/java/com/example/austin/harrypotterrev2/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -15,6 +17,13 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     ArrayList<Movie> movieDescirption;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.option_menu, menu);
+        return true;
+    }
 
 
     @Override
