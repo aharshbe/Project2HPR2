51ab05f651215f8cd566c8db52adfde0fda05eb5
aharshbe <a.catawampus@gmail.com>
2016-05-03 18:12:13 -0700

    Added first movie

--- harrypotterrev2project2/app/src/main/java/com/example/austin/harrypotterrev2/DatabaseHelper.java
+++ harrypotterrev2project2/app/src/main/java/com/example/austin/harrypotterrev2/DatabaseHelper.java
@@ -13,7 +13,7 @@ import android.database.sqlite.SQLiteOpenHelper;
  */
 public class DatabaseHelper extends SQLiteOpenHelper {
     // Define the database name and version
-    public static final int DATABASE_VERSION = 13;
+    public static final int DATABASE_VERSION = 14;
     public static final String DATABASE_NAME = "movies.db";
 
     //Creating variables for each of the columns
@@ -79,12 +79,12 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         ContentValues values = new ContentValues();
 
         //Movie one
-        values.put(COL_PLOT, "this is the plot");
-        values.put(COL_TITLE, "some tdsfsfsfitle");
-        values.put(COL_DATE, "some date");
-        values.put(COL_RUNTIME, "some runtime");
-        values.put(COL_TOPQUOTE, "some quote");
-        values.put(COL_GROSS, "some gross");
+        values.put(COL_PLOT, "This is the tale of Harry Potter, an ordinary 11-year-old boy serving as a sort of slave for his aunt and uncle who learns that he is actually a wizard and has been invited to attend the Hogwarts School for Witchcraft and Wizardry. Harry is snatched away from his mundane existence by Hagrid, the grounds keeper for Hogwarts, and quickly thrown into a world completely foreign to both him and the viewer. Famous for an incident that happened at his birth, Harry makes friends easily at his new school. He soon finds, however, that the wizarding world is far more dangerous for him than he would have imagined, and he quickly learns that not all wizards are ones to be trusted. Written by Carly");
+        values.put(COL_TITLE, "Harry Potter and the Sorcerer's Stone");
+        values.put(COL_DATE, "16 November 2001 (USA)");
+        values.put(COL_RUNTIME, "152 min | 159 min (extended)");
+        values.put(COL_TOPQUOTE, "'It does not do to dwell on dreams and forget to live.' — Harry Potter and the Sorcerer's Stone, Dumbledore");
+        values.put(COL_GROSS, "$317,557,891 (USA) (17 May 2002)");
         db.insert(MOVIES_TABLE_NAME, null, values);
         //Movie 2
         values = new ContentValues();
