d69b2fda8dd39293c9ba548f322c753e07263f96
aharshbe <a.catawampus@gmail.com>
2016-05-04 16:13:23 -0700

    Adding switchase

---
+++ harrypotterrev2project2/app/src/main/java/com/example/austin/harrypotterrev2/AddingImages.java
@@ -0,0 +1,39 @@
+package com.example.austin.harrypotterrev2;
+
+/**
+ * Created by austin on 5/4/16.
+ */
+public class AddingImages {
+    public static int getDrawable(String image){
+        switch (image){
+            case "movie1":
+                return R.drawable.hpv1sor;
+            case "movie2":
+                return R.drawable.hpv22;
+            case "movie3":
+                return R.drawable.hpmv3movie;
+            case "movie4":
+                return R.drawable.hpvmv4;
+            case "movie5":
+                return R.drawable.hpvmvc5;
+            case "movie6":
+                return R.drawable.hpvmv6;
+            case "movie7":
+                return R.drawable.hpmv7;
+            case "movie8":
+                return R.drawable.hpmv8;
+            default:
+                return 0;
+
+
+
+
+
+
+
+
+
+        }
+
+    }
+}
