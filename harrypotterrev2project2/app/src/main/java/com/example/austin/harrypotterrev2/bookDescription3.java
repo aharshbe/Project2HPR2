6a3aeb09f53e47b7c2f4a54238eb9af64a97b8ba
aharshbe <a.catawampus@gmail.com>
2016-05-05 11:02:21 -0700

    Adding the third book

--- harrypotterrev2project2/app/src/main/java/com/example/austin/harrypotterrev2/bookDescription3.java
+++ harrypotterrev2project2/app/src/main/java/com/example/austin/harrypotterrev2/bookDescription3.java
@@ -1,7 +1,9 @@
 package com.example.austin.harrypotterrev2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class bookDescription3 extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class bookDescription3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_description3);
+    }
+    public void booksback3(View view) {
+        Intent intent = new Intent(bookDescription3.this, BooksActivity.class);
+        startActivity(intent);
     }
 }
