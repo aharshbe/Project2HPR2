b87968dc0a3853e791da1ddbcf311642cac61c8a
aharshbe <a.catawampus@gmail.com>
2016-05-04 17:16:27 -0700

    Adding the coverpage

--- harrypotterrev2project2/app/src/main/java/com/example/austin/harrypotterrev2/CoverPage.java
+++ harrypotterrev2project2/app/src/main/java/com/example/austin/harrypotterrev2/CoverPage.java
@@ -1,7 +1,9 @@
 package com.example.austin.harrypotterrev2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class CoverPage extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class CoverPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cover_page);
+    }
+
+    public void clickingTextView(View view) {
+        Intent intent = new Intent(CoverPage.this, MainActivity.class);
+        startActivity(intent);
     }
 }
