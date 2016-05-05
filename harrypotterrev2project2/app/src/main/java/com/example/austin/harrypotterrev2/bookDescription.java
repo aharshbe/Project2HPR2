dfef4b6c8d4e2e59f3d2dc91fa2380971e71a1e4
aharshbe <a.catawampus@gmail.com>
2016-05-05 10:58:27 -0700

    Added for first book

--- harrypotterrev2project2/app/src/main/java/com/example/austin/harrypotterrev2/bookDescription.java
+++ harrypotterrev2project2/app/src/main/java/com/example/austin/harrypotterrev2/bookDescription.java
@@ -1,7 +1,9 @@
 package com.example.austin.harrypotterrev2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class bookDescription extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class bookDescription extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_description);
+    }
+
+
+    public void booksback(View view) {
+        Intent intent = new Intent(bookDescription.this, BooksActivity.class);
+        startActivity(intent);
     }
 }
