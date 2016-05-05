e6d8228aa205ded583676f20f50fc95ef4d1a237
aharshbe <a.catawampus@gmail.com>
2016-05-05 11:55:44 -0700

    Fixing dobby

--- harrypotterrev2project2/app/src/main/java/com/example/austin/harrypotterrev2/dobbyDescription.java
+++ harrypotterrev2project2/app/src/main/java/com/example/austin/harrypotterrev2/dobbyDescription.java
@@ -1,9 +1,11 @@
 package com.example.austin.harrypotterrev2;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class dobbyDescription extends AppCompatActivity {
 
@@ -11,6 +13,10 @@ public class dobbyDescription extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dobby_description);
+
+        Typeface font = Typeface.createFromAsset(getAssets(), "hpfont.TTF");
+        TextView dobby = (TextView) findViewById(R.id.dobby);
+        dobby.setTypeface(font);
     }
     public void clickingdobby(View view) {
         Intent intent = new Intent(dobbyDescription.this, QuotesActivity.class);
