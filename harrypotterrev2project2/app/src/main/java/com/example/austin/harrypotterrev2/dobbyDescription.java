e34200167a4b2b520eb047fb409bcc71a3e5230e
aharshbe <a.catawampus@gmail.com>
2016-05-05 11:33:15 -0700

    got dumbledore, starting dobby

--- harrypotterrev2project2/app/src/main/java/com/example/austin/harrypotterrev2/dobbyDescription.java
+++ harrypotterrev2project2/app/src/main/java/com/example/austin/harrypotterrev2/dobbyDescription.java
@@ -1,7 +1,9 @@
 package com.example.austin.harrypotterrev2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class dobbyDescription extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class dobbyDescription extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dobby_description);
+    }
+    public void clickingdobby(View view) {
+        Intent intent = new Intent(dobbyDescription.this, QuotesActivity.class);
+        startActivity(intent);
     }
 }
--- harrypotterrev2project2/app/src/main/java/com/example/austin/harrypotterrev2/quoteDescription.java
+++ harrypotterrev2project2/app/src/main/java/com/example/austin/harrypotterrev2/quoteDescription.java
@@ -1,7 +1,9 @@
 package com.example.austin.harrypotterrev2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class quoteDescription extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class quoteDescription extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quote_description);
+    }
+    public void clickingdumbledore(View view) {
+        Intent intent = new Intent(quoteDescription.this, QuotesActivity.class);
+        startActivity(intent);
     }
 }
