04290aaa59ffbfe1a42db059824eae146cc3f6eb
aharshbe <a.catawampus@gmail.com>
2016-05-05 11:46:18 -0700

    Fixing the text view over to text on the quotes page

--- harrypotterrev2project2/app/src/main/java/com/example/austin/harrypotterrev2/QuotesActivity.java
+++ harrypotterrev2project2/app/src/main/java/com/example/austin/harrypotterrev2/QuotesActivity.java
@@ -1,9 +1,11 @@
 package com.example.austin.harrypotterrev2;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class QuotesActivity extends AppCompatActivity {
 
@@ -11,6 +13,17 @@ public class QuotesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quotes);
+
+        Typeface font = Typeface.createFromAsset(getAssets(), "hpfont.TTF");
+        TextView quotes = (TextView) findViewById(R.id.quotetitle);
+        TextView quote1 = (TextView) findViewById(R.id.quote1);
+        TextView quote2 = (TextView) findViewById(R.id.quote2);
+        TextView quote3 = (TextView) findViewById(R.id.quote3);
+        quote1.setTypeface(font);
+        quote2.setTypeface(font);
+        quote3.setTypeface(font);
+        quotes.setTypeface(font);
+
     }
 
     //Adds intent over to main acitivty from quotes
