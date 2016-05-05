b79342cb016707639ac3ec30e9ba8a483f801e9c
aharshbe <a.catawampus@gmail.com>
2016-05-05 11:52:36 -0700

    Fixing snape

--- harrypotterrev2project2/app/src/main/java/com/example/austin/harrypotterrev2/quoteDescription.java
+++ harrypotterrev2project2/app/src/main/java/com/example/austin/harrypotterrev2/quoteDescription.java
@@ -1,9 +1,11 @@
 package com.example.austin.harrypotterrev2;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class quoteDescription extends AppCompatActivity {
 
@@ -11,6 +13,13 @@ public class quoteDescription extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quote_description);
+
+        Typeface font = Typeface.createFromAsset(getAssets(), "hpfont.TTF");
+        TextView dumble = (TextView) findViewById(R.id.textduble);
+        dumble.setTypeface(font);
+
+
+
     }
     public void clickingdumbledore(View view) {
         Intent intent = new Intent(quoteDescription.this, QuotesActivity.class);
--- harrypotterrev2project2/app/src/main/java/com/example/austin/harrypotterrev2/snapeDescription.java
+++ harrypotterrev2project2/app/src/main/java/com/example/austin/harrypotterrev2/snapeDescription.java
@@ -1,9 +1,11 @@
 package com.example.austin.harrypotterrev2;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class snapeDescription extends AppCompatActivity {
 
@@ -11,6 +13,12 @@ public class snapeDescription extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_snape_description);
+
+        Typeface font = Typeface.createFromAsset(getAssets(), "hpfont.TTF");
+        TextView snape = (TextView) findViewById(R.id.snape);
+        snape.setTypeface(font);
+
+
     }
     public void clickingsnape(View view) {
         Intent intent = new Intent(snapeDescription.this, QuotesActivity.class);
