fa409a33756b80f32f2de28b9047c37ae7906fe0
aharshbe <a.catawampus@gmail.com>
2016-05-05 17:25:23 -0700

    Adding last part

--- harrypotterrev2project2/app/src/main/java/com/example/austin/harrypotterrev2/easteregg.java
+++ harrypotterrev2project2/app/src/main/java/com/example/austin/harrypotterrev2/easteregg.java
@@ -1,9 +1,15 @@
 package com.example.austin.harrypotterrev2;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.SurfaceView;
 import android.view.View;
+
+
+import java.io.InputStream;
 
 public class easteregg extends AppCompatActivity {
 
@@ -12,7 +18,8 @@ public class easteregg extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_easteregg);}
 
-        //Creates intent for user so they can go back to main screen
+
+    //Creates intent for user so they can go back to main screen
 
     public void backtocategories(View view) {
         Intent intent = new Intent(easteregg.this, CoverPage.class);
