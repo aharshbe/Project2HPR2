48c5a5f2b5db9bebd9c3b08f230e93e2ad525073
aharshbe <a.catawampus@gmail.com>
2016-05-06 10:47:40 -0700

    Adding the helper object class for the sounds

---
+++ harrypotterrev2project2/app/src/main/java/com/example/austin/harrypotterrev2/AddingSound.java
@@ -0,0 +1,17 @@
+package com.example.austin.harrypotterrev2;
+
+/**
+ * Created by austin on 5/6/16.
+ */
+public class AddingSound {
+    public static int getDrawable(String sound) {
+        switch (sound) {
+            case "sound1":
+                return R.drawable.ilym;
+            default:
+                return 0;
+
+        }
+
+    }
+}
