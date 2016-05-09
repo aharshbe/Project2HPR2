5a619002a8baeab4b5140309d625313171bbdb87
aharshbe <a.catawampus@gmail.com>
2016-05-09 15:49:08 -0700

    Adding photos category

--- harrypotterrev2project2/app/src/main/java/com/example/austin/harrypotterrev2/NavBar.java
+++ harrypotterrev2project2/app/src/main/java/com/example/austin/harrypotterrev2/NavBar.java
@@ -97,6 +97,10 @@ public class NavBar extends AppCompatActivity
 
         } else if (id == R.id.nav_send) {
 
+        } else if (id == R.id.gallery) {
+
+        } else if (id == R.id.slideshow) {
+
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
