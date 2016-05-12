be8ec5823c20e3142b1fb5b39bc3ab31aa8d238d
aharshbe <a.catawampus@gmail.com>
2016-05-12 15:57:49 -0700

    Got the animation to work

--- harrypotterrev2project2/app/src/main/java/com/example/austin/harrypotterrev2/easteregg.java
+++ harrypotterrev2project2/app/src/main/java/com/example/austin/harrypotterrev2/easteregg.java
@@ -7,6 +7,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.SurfaceView;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 
 import java.io.InputStream;
@@ -14,7 +17,11 @@ import java.io.InputStream;
 //Just a random class I created for a fun little easteregg, originally it was suppose to also have a moving gif and a sound,
 //Sadly I wasn't able to implement either of those two, though I did try in my various branch :P
 
+
+
 public class easteregg extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +45,14 @@ public class easteregg extends AppCompatActivity {
     public void clicksound234(View view) {
         Intent intent = new Intent(easteregg.this, tryingSound.class);
         startActivity(intent);
+    }
+
+    public void clickingSpinner(View view) {
+        Animation animation = null;
+        ImageView imageView = (ImageView) findViewById(R.id.moldy);
+        animation = AnimationUtils.loadAnimation(this, R.anim.movingegg);
+        imageView.startAnimation(animation);
+
     }
 }
 
