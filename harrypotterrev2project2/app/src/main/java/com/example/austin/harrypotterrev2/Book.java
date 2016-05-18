4c990d9997ea383f1e6aaf24070757196d21eeaf
aharshbe <a.catawampus@gmail.com>
2016-05-17 17:13:36 -0700

    Adding the Book object which contains global variables and the getter/setters

---
+++ harrypotterrev2project2/app/src/main/java/com/example/austin/harrypotterrev2/Book.java
@@ -0,0 +1,79 @@
+package com.example.austin.harrypotterrev2;
+
+/**
+ * Created by austin on 5/17/16.
+ */
+public class Book {
+        private String mTitle;
+        private String mPlot;
+        private String mDate;
+        private String mPages;
+        private String mQuote;
+        private String mFavChar;
+        private String mId;
+        private String mCover;
+
+    public String getmTitle() {
+        return mTitle;
+    }
+
+    public void setmTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+
+    public String getmPlot() {
+        return mPlot;
+    }
+
+    public void setmPlot(String mPlot) {
+        this.mPlot = mPlot;
+    }
+
+    public String getmDate() {
+        return mDate;
+    }
+
+    public void setmDate(String mDate) {
+        this.mDate = mDate;
+    }
+
+    public String getmPages() {
+        return mPages;
+    }
+
+    public void setmPages(String mPages) {
+        this.mPages = mPages;
+    }
+
+    public String getmQuote() {
+        return mQuote;
+    }
+
+    public void setmQuote(String mQuote) {
+        this.mQuote = mQuote;
+    }
+
+    public String getmFavChar() {
+        return mFavChar;
+    }
+
+    public void setmFavChar(String mFavChar) {
+        this.mFavChar = mFavChar;
+    }
+
+    public String getmId() {
+        return mId;
+    }
+
+    public void setmId(String mId) {
+        this.mId = mId;
+    }
+
+    public String getmCover() {
+        return mCover;
+    }
+
+    public void setmCover(String mCover) {
+        this.mCover = mCover;
+    }
+}
